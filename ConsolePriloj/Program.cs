5dcc2feb229bd618f9b68587267877cc437745ee
Cab38-01 <Cab38-01@03-C38-01>
2021-06-18 12:15:51 +0300

    Добавьте файлы проекта.

---
+++ ConsolePriloj/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsolePriloj
+{
+    class Program
+    {
+        static void Main( )
+        {
+            Console.WriteLine(string.Join("", Console.ReadLine().ToArray().Reverse()));
+        }
+    }
+}
